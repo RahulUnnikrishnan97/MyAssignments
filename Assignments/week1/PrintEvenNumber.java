110c924995b12561046d116e9e9fb7b617f41d2f
RahulUnnikrishnan97 <168268760+RahulUnnikrishnan97@users.noreply.github.com>
2024-05-15 10:52:11 +0530

    Add files via upload

---
+++ Assignments/week1/PrintEvenNumber.java
@@ -0,0 +1,23 @@
+package Assignments.week1;
+
+public class PrintEvenNumber 
+
+{
+	
+	public static void main(String[] args) {
+		
+		
+for (int i = 1; i<=30; i++) 
+	
+{
+			
+			if (i%2==0) {
+			System.out.println("The even numbers are "+i);
+		}
+		
+		
+		
+		
+	}}}
+
+
---
+++ Assignments/week3/Inheritance_TestData.java
@@ -0,0 +1,5 @@
+package Assignments.week3;
+
+public class Inheritance_TestData {
+
+}
